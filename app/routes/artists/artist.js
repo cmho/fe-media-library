527cf2fb192d73d2e0b58987a25a12ee2fe2e2cb
Carly Ho <carlymho@gmail.com>
2015-11-23 16:31:50 -0600

    Comment out offending line for now

--- app/routes/artists/artist.js
+++ app/routes/artists/artist.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
 	model(params) {
 		return Ember.RSVP.hash({
 			artist: this.store.peekRecord('artist', params.artist_id),
-			album: this.store.query('album', {filter: {artist_id: params.artist_id}})
+			//album: this.store.query('album', {filter: {artist_id: params.artist_id}})
 		});
 	}
 });
